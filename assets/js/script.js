5e5be0edc6824eb4e3faa5252ad66c0fdbd9ca86
shannon_p <snp1284kasey@aol.com>
2021-07-11 17:54:34 -0400

    add ability to edit dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Stopped at the beginning of 5.1.7
 var tasks = {};
 
 var createTask = function(taskText, taskDate, taskList) {
@@ -45,6 +46,57 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+  .text()
+  .trim();
+  
+  // create new input element
+  var dateInput = $("<input>")
+  .attr("type", "text")
+  .addClass("form-control")
+  .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text;]", function() {
+  // get current text
+  var date = $(this)
+  .val()
+  .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+  .closet(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+   var index = $(this)
+   .closet(".list-group-item")
+   .index();
+
+   // update task in array and re-save to localStorage
+   tasks[status][index].date = date;
+   saveTasks();
+
+   // recreate span element with bootstrap classes
+   var TaskSpan = $("<span>")
+   .addClass("badge badge-primary badge-pill")
+   .text(date);
+
+   // replace input with span element
+   $(this).replaceWith(taskSpan);
+});
+
 $(".list-group").on("click", "p", function() {
   var text = $(this)
   .text()
@@ -58,7 +110,7 @@ $(".list-group").on("click", "p", function() {
 });
 
 $(".list-group").on("blur", "textarea", function() {
-  // get the textarea's current value/text
+  // get the text area's current value/text
   var text = $(this)
   .val()
   .trim();
